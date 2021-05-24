88b6e113db8a6df5d3839cd682dc72415ab6d7d3
Tatiana <wypick@gmail.com>
2021-05-24 19:49:57 +0300

    .

--- Autotests/Utils.cs
+++ Autotests/Utils.cs
@@ -13,7 +13,7 @@ namespace Autotests
 
         public static Uri GetUri(string path)
         {
-            Uri uri = new Uri($"https://{host}");
+            Uri uri = new Uri($"http://{host}");
             return uri;
         }
 
